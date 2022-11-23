88014fac92369e99e9d13f41130fa66046697ea5
EL <ahmedgarnousy76@gmail.com>
2022-11-23 18:12:18 +0200

    Printing Homework done

---
+++ 170 problems/01_printing/Printing.cpp
@@ -0,0 +1,54 @@
+//============================================================================
+// Name        : Printing.cpp
+// Author      : Ahmed Kamal Hussein
+// Version     : v01
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+ cout<<"work smart not hard \n";
+
+	/*
+	Homework 2 sol 1#: Write code to print the following
+	A) A triangle with angel 90 degree
+	*/
+
+    cout<<"*\n**\n***\n****\n*****";
+    cout<<endl;
+	/*Homework 2 sol 2*/
+	cout<<"*"<<endl<<
+		  ""	;
+
+	cout<<"    *\n" "   ***\n  *****\n *******\n*********";
+    cout<<endl;
+
+	cout<<"   *   "<<endl<<
+		  "  ***  "<<endl<<
+		  " ***** "<<endl<<
+		  "  ***  "<<endl<<
+		  "   *   ";
+    cout<<endl;
+
+	/*Homework#1 sol 1 *2*/
+
+    cout<<"    1\n" "   232\n""  34543\n"" 4567654\n""567898765\n";
+    cout<<endl;
+
+    cout<<"            1\n" "          1   1\n""        1   2   1\n""      1   3   3   1\n""    1  4   6   4    1\n""   1  5   10   10  5  1";
+    cout<<endl;
+
+	   cout<<"children must be taught how to think , not what to think ";
+	   cout<<" we worry about what will become tomorrow, yet we forget that he is someone today \n";
+	   cout<<" Children are not things to be molded < but are people to be unfolded\n";
+	   cout<<" It is easier to build strong children than to repair broken men";
+	   cout <<" children have never been very good at listening to their elders, but they have never failed to imitate them ";
+	   cout<<"children are our most valuable resource\n";
+	       return 0;
+
+    return 0;
+}
